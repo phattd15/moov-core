28135db71d8aa0d4b6f7cd4cf1559f00d0da52c2
Phat Tran <futanaristic@gmail.com>
2022-04-27 22:55:56 +0900

    Adding split data

--- util/fetchData.js
+++ util/fetchData.js
@@ -1,5 +1,11 @@
 const axios = require('axios');
 
-const fetchData = () => {
-  const url
+const fetchData = async () => {
+  const url = "https://raw.githubusercontent.com/polarity-cf/moov-core/master/dataset_split.json";
+
+  let request = await axios.get(url);
+
+  console.log(request.data);
 }
+
+fetchData()
